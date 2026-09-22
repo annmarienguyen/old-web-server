8eac325bf8a83035204f6d5530862efd7634f26b
Ann Marie Nguyen <annmarienguy@umass.edu>
2026-09-22 05:30:27 +0000

    Unit work before Lab 2

---
+++ routes/api.js
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+
+const router = Router();
+
+router.get('/info', (req, res) => {
+  res.json({ version: '1.0', status: 'ok' });
+});
+
+router.get('/status', (req, res) => {
+  const status = {status: "ok"};
+  res.json(status);
+})
+
+export default router;
